50cd3b23f506c857681228e7fd6cc5efcc8fd0b2
Edward Z. Yang <ezyang@fb.com>
2018-01-26 08:19:36 -0800

    Port cuDNN RNNs to ATen.
    This brings three new functions:
      - _cudnn_rnn_flatten_weight: flatten a matrix of weight tensors into
        a single contiguous weight buffer as required by cuDNN
      - _cudnn_rnn: run RNN forwards
      - _cudnn_rnn_backward: run RNN backwards
    
    RNNs have a lot of parameters, so we restructured what was previously
    a single 'fn' object that recorded all the parameters into three
    objects: RNNDescriptorParams, TensorDescriptorListParams and
    DropoutDescriptorParams.
    
    We make use of MatrixRef to organize the weight tensors (which are
    weight/bias x number of layers), but I did not teach the codegen
    how to pass these as arguments/return values natively, so instead
    a MatrixRef is passed as its constituent ArrayRef and int64_t stride0.
    
    cudnn_rnn has three differentiable outputs and one nondifferentiable
    one, so it makes use of the support for hard-coded differentiable outputs.
    
    I haven't deleted all of the descriptor code from Python, because dropout
    initialization still goes through this codepath, that should be fixed soon
    but I don't see it as essential for this PR.
    
    This commit also removes the last use of NestedIOFunction from PyTorch.
    
    There are some shenanigans with cuDNN dropout descriptor initialization,
    see below:
    
    Note [cuDNN dropout descriptor initialization]
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    In most cases, setting descriptors in cuDNN is cheap (e.g.,
    cudnnSetTensorNdDescriptor).  However, this is not the case for
    cudnnSetDropoutDescriptor: in cuDNN 6/7 (and possibly others) it does an
    expensive precomputation to initialize the random number generator states.  In
    cuDNN 6, this is the ONLY official mechanism to initialize a dropout descriptor,
    which means that law-abiding clients were expected to generate a dropout
    descriptor once and cache it.  However, our ATen interface is (1) stateless (so
    we can't cache the descriptors) and (2) does not accept arbitrary user types in
    its interface (so we can't pass the descriptor in).  This puts us in a pickle.
    
    In cuDNN 7, a new function, cudnnRestoreDropoutDescriptor was added, which
    forgoes the expensive initialization process, and can initialize the
    descriptor with a pre-initialized state CUDA tensor.  This is great, because
    it means we can simply pass in the state tensor and then initialize the
    descriptor internally.  Unfortunately, this function is not available in
    cuDNN 6.
    
    To work around this, we break the cuDNN abstraction barrier, and have
    the struct layout of the underlaying dropout descriptor.  With this struct,
    we can reimplement cudnnRestoreDropoutDescriptor from scratch. Great!
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Fix cuDNN 7 behavior.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Add missing filter_dim_a slice
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Try more efficient flatten.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    Hacky version with contiguous array
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- aten/src/ATen/cudnn/Descriptors.h
+++ aten/src/ATen/cudnn/Descriptors.h
@@ -3,8 +3,45 @@
 #include "Exceptions.h"
 
 #include "cudnn-wrapper.h"
-#include <ATen/Tensor.h>
+#include <ATen/ATen.h>
 #include <ATen/Check.h>
+
+#if CUDNN_VERSION < 7000
+
+/*
+Note [cuDNN dropout descriptor initialization]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+In most cases, setting descriptors in cuDNN is cheap (e.g.,
+cudnnSetTensorNdDescriptor).  However, this is not the case for
+cudnnSetDropoutDescriptor: in cuDNN 6/7 (and possibly others) it does an
+expensive precomputation to initialize the random number generator states.  In
+cuDNN 6, this is the ONLY official mechanism to initialize a dropout descriptor,
+which means that law-abiding clients were expected to generate a dropout
+descriptor once and cache it.  However, our ATen interface is (1) stateless (so
+we can't cache the descriptors) and (2) does not accept arbitrary user types in
+its interface (so we can't pass the descriptor in).  This puts us in a pickle.
+
+In cuDNN 7, a new function, cudnnRestoreDropoutDescriptor was added, which
+forgoes the expensive initialization process, and can initialize the
+descriptor with a pre-initialized state CUDA tensor.  This is great, because
+it means we can simply pass in the state tensor and then initialize the
+descriptor internally.  Unfortunately, this function is not available in
+cuDNN 6.
+
+To work around this, we break the cuDNN abstraction barrier, and have
+the struct layout of the underlaying dropout descriptor.  With this struct,
+we can reimplement cudnnRestoreDropoutDescriptor from scratch. Great!
+*/
+
+// Reverse engineered from cuDNN 6, see Note [cuDNN dropout descriptor initialization]
+struct cudnnDropoutStruct {
+  float dropout;
+  int nstates;
+  void * states;
+};
+
+#endif
 
 namespace at { namespace native {
 
@@ -111,6 +148,36 @@ private:
 
 std::ostream& operator<<(std::ostream & out, const TensorDescriptor& d);
 
+// Sigh
+class TensorDescriptorArray
+{
+  std::vector<cudnnTensorDescriptor_t> arr;
+public:
+  TensorDescriptorArray() : arr() {}
+  TensorDescriptorArray& operator=(TensorDescriptorArray&& x) {
+    std::swap(arr, x.arr);
+    return *this;
+  }
+  TensorDescriptorArray(TensorDescriptorArray&& x) {
+    std::swap(arr, x.arr);
+  }
+  ~TensorDescriptorArray() {
+    for (auto ptr : arr) {
+      CUDNN_CHECK(cudnnDestroyTensorDescriptor(ptr));
+    }
+  }
+  cudnnTensorDescriptor_t* descs() {
+    return arr.data();
+  }
+  void set_packed(const at::Tensor& t, IntList batch_sizes);
+  void set_unpacked(const at::Tensor& t, int64_t seq_length);
+
+private:
+  void set_raw(size_t i, cudnnDataType_t dataType, int dim, int* size, int* stride) {
+    CUDNN_CHECK(cudnnSetTensorNdDescriptor(arr[i], dataType, dim, size, stride));
+  }
+};
+
 class FilterDescriptor
   : public Descriptor<cudnnFilterStruct,
                       &cudnnCreateFilterDescriptor,
@@ -151,6 +218,103 @@ struct SpatialTransformerDescriptor
 {
   void set(cudnnDataType_t dataType, int dim, int* size) {
     CUDNN_CHECK(cudnnSetSpatialTransformerNdDescriptor(mut_desc(), CUDNN_SAMPLER_BILINEAR, dataType, dim, size));
+  }
+};
+
+#if CUDNN_VERSION < 7000
+
+inline cudnnStatus_t cudnnRestoreDropoutDescriptor(
+    cudnnDropoutDescriptor_t dropoutDesc,
+    cudnnHandle_t handle,
+    float dropout,
+    void *states,
+    size_t stateSizeInBytes,
+    unsigned long long seed) {
+  // Try to accurately simulate cuDNN's behavior, for our cuDNN 6 friends
+  if (states == nullptr) return CUDNN_STATUS_INVALID_VALUE;
+  if (stateSizeInBytes == 0) return CUDNN_STATUS_INVALID_VALUE;
+  dropoutDesc->dropout = dropout;
+  dropoutDesc->nstates = stateSizeInBytes;
+  dropoutDesc->states = states;
+  return CUDNN_STATUS_SUCCESS;
+}
+
+#endif // CUDNN_VERSION
+
+struct DropoutDescriptor
+  : public Descriptor<cudnnDropoutStruct,
+                      &cudnnCreateDropoutDescriptor,
+                      &cudnnDestroyDropoutDescriptor>
+{
+  at::Tensor state;
+
+  // This is expensive, avoid calling me!
+  void expensiveSet(cudnnHandle_t handle, float dropout, long long int seed) {
+    void *state_ptr = nullptr;
+    size_t state_size = 0;
+    if (dropout > 0) {
+      size_t dropout_state_size;
+      CUDNN_CHECK(cudnnDropoutGetStatesSize(handle, &dropout_state_size));
+      state = at::CUDA(kByte).tensor({static_cast<int64_t>(dropout_state_size)});
+      state_ptr = state.data_ptr();
+      state_size = state.size(0);
+    }
+    CUDNN_CHECK(cudnnSetDropoutDescriptor(mut_desc(), handle, dropout, state_ptr, state_size, seed));
+  }
+
+  // I'm cheap! Call me!
+  // See Note [cuDNN dropout descriptor initialization]
+  void set(cudnnHandle_t handle, float dropout, at::Tensor state_, long long int seed) {
+    if (dropout > 0) {
+      state = state_;
+      void *state_ptr = state.data_ptr();
+      size_t state_size = state.size(0);
+      CUDNN_CHECK(cudnnRestoreDropoutDescriptor(mut_desc(), handle, dropout, state_ptr, state_size, seed));
+    } else {
+      // Empirically, expensiveSet is cheap when dropout is 0
+      expensiveSet(handle, dropout, seed);
+    }
+  }
+};
+
+struct RNNDescriptor
+  : public Descriptor<cudnnRNNStruct,
+                      &cudnnCreateRNNDescriptor,
+                      &cudnnDestroyRNNDescriptor>
+{
+  DropoutDescriptor dropout_desc_;
+  void set(cudnnHandle_t handle, int hidden_size, int num_layers, DropoutDescriptor&& dropout_desc,
+           cudnnRNNInputMode_t input_mode, cudnnDirectionMode_t bidirectional,
+           cudnnRNNMode_t mode, cudnnDataType_t datatype) {
+    dropout_desc_ = std::move(dropout_desc);
+    CUDNN_CHECK(cudnnSetRNNDescriptor_v6(
+          handle,
+          mut_desc(),
+          hidden_size,
+          num_layers,
+          dropout_desc_.desc(),
+          input_mode,
+          bidirectional,
+          mode,
+          CUDNN_RNN_ALGO_STANDARD,
+          datatype));
+#if CUDNN_VERSION >= 7000 && CUDA_VERSION >= 9000
+    // TODO: This code should live as a utility somewhere in ATen.
+    // Please don't copy paste me!
+    int device;
+    CUDA_CHECK(cudaGetDevice(&device));
+    cudaDeviceProp prop;
+    CUDA_CHECK(cudaGetDeviceProperties(&prop, device));
+    if (prop.major >= 7) {
+      if (datatype == CUDNN_DATA_HALF) {
+        cudnnSetRNNMatrixMathType(mut_desc(), CUDNN_TENSOR_OP_MATH);
+      } else {
+        // Technically, as the default it's not necessary to explicitly
+        // set this.
+        cudnnSetRNNMatrixMathType(mut_desc(), CUDNN_DEFAULT_MATH);
+      }
+    }
+#endif
   }
 };
 
