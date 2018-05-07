e5fc4e081b2fd81a85cf99787c3624b797d5863d
Jenkins <jenkins@ci.pytorch.org>
2018-05-07 00:10:48 +0000

    More fixes
    Signed-off-by: Jenkins <jenkins@ci.pytorch.org>

--- aten/src/ATen/native/ReduceOps.cpp
+++ aten/src/ATen/native/ReduceOps.cpp
@@ -122,10 +122,6 @@ Tensor _prod_cpu(const Tensor &self) {
   return self._prodall();
 }
 
-Tensor _sum_cuda(const Tensor &self_) { return self_._sumall(); }
-
-Tensor _prod_cuda(const Tensor &self_) { return self_._prodall(); }
-
 // \ALL REDUCE ################################################################
 
 // DIM REDUCE #################################################################
@@ -221,16 +217,6 @@ Tensor &_prod_out_cpu(Tensor &result, const Tensor &self, int64_t dim_,
     if (!keepdim) result.squeeze_(dim);
     return result;
   }
-  return at::_th_prod_out(result, self, dim, keepdim);
-}
-
-Tensor &_sum_out_cuda(Tensor &result, const Tensor &self, int64_t dim,
-                      bool keepdim) {
-  return at::_th_sum_out(result, self, dim, keepdim);
-}
-
-Tensor &_prod_out_cuda(Tensor &result, const Tensor &self, int64_t dim,
-                       bool keepdim) {
   return at::_th_prod_out(result, self, dim, keepdim);
 }
 
