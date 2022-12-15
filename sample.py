074009c9948d56183e9175a2c8dc7d7771d7f7ac
Edward Z. Yang <ezyang@fb.com>
2022-12-15 10:43:04 +0800

    sample
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- sample.py
+++ sample.py
@@ -26,6 +26,50 @@ class TensorMeta(NamedTuple):
 # - Hash consing to CSE as we go
 # - Alternate output format that's not FX (binary maybe?)
 
+symbols = {
+    'neg': '-',
+    'sub': '-',
+    'add': '+',
+    'mul': '*',
+    'floordiv': 'div',
+    'mod': 'mod',
+    'eq': '=',
+    'ne': 'distinct',
+    'le': '<=',
+    'lt': '<',
+    'ge': '>=',
+    'gt': '>',
+    'not_': 'not',
+}
+
+"""
+    'pow': '**',
+
+    'lshift': '<<',
+    'rshift': '>>',
+    'and_': '&',
+    'or_': '|',
+    'xor': '^',
+
+    'div': '/',
+    'truediv': '/',
+    'pos': '+',
+    'invert': '~'
+"""
+
+def render(t):
+    if t.__module__ == '_operator' and t.__name__ in symbols:
+        return symbols[t.__name__]
+    else:
+        raise AssertionError(f"unknown {t.__name__}")
+        #return t.__name__
+
+def V(x):
+    if isinstance(x, torch.fx.Node):
+        return x.name
+    else:
+        return str(x)
+
 def my_compiler(gm, example_inputs):
     fake_mode = fake_mode_from_tensors(example_inputs)
     # TODO: get this from tracing context
@@ -66,7 +110,38 @@ def my_compiler(gm, example_inputs):
     # Better not to DCE
     # shape_graph.eliminate_dead_code()
 
-    torch.fx.GraphModule({}, shape_graph).print_readable()
+    print("(set-option :produce-models true)")
+    get_values = []
+    for n in shape_graph.nodes:
+        if n.op == "placeholder":
+            get_values.append(n.name)
+            print(f"(declare-const {n.name} Int)")
+        elif n.op == "call_function":
+            if n.target is torch._assert:
+                print(f"(assert {n.args[0].name})")
+            else:
+                # TODO: fold single use nodes
+                ty = "Int"
+                if n.target.__name__ in ['eq', 'ne', 'le', 'lt', 'ge', 'gt', 'not_']:
+                    ty = "Bool"
+                print(f"(define-fun {n.name} () {ty} ({render(n.target)} {' '.join(V(a) for a in n.args)}))")
+        elif n.op == "output":
+            for i, o in enumerate(n.args[0]):
+                for j, s in enumerate(o.size):
+                    print(f"(define-fun output{i}_size{j} () Int {V(s)})")
+                    get_values.append(f"output{i}_size{j}")
+                for j, s in enumerate(o.stride):
+                    print(f"(define-fun output{i}_stride{j} () Int {V(s)})")
+                    get_values.append(f"output{i}_stride{j}")
+                print(f"(define-fun output{i}_storage_offset () Int {V(o.storage_offset)})")
+                get_values.append(f"output{i}_storage_offset")
+        else:
+            raise AssertionError(n.op)
+    print("(check-sat)")
+    print("(get-model)")
+    #print(f"(get-values ({' '.join(get_values)}))")
+
+    #torch.fx.GraphModule({}, shape_graph).print_readable()
     #print(gm)
     #print(example_inputs)
     #for g in fake_mode.shape_env.fx_guards:
